d257b000ac352f097f60f3c92e7d593811afb656
Michael Vinogradov <mr.feni@gmail.com>
2015-05-21 10:14:01 +0300

    no message

---
+++ checkip.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function checkip()
+{
+ip=""
+attempt=0
+while [ "$ip" = "" ]; do
+        attempt=$(($attempt+1))
+        ip=`curl http://geoip.hidemyass.com/ip/ 2>/dev/null`
+        if [ "$ip" != "" ]; then
+                echo "- Your current IP is: $ip"
+        fi
+        if [ $attempt -gt 3 ]; then
+                echo "- Failed to check current IP address."
+                exit
+        fi
+done
+}
+
+checkip
